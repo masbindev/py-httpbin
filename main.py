35bde3d83a6aeb5763a82fe95111e18a60df2bfe
Cahya Bintarum <masbindev@gmail.com>
2023-05-19 22:36:24 +0700

    integrate tailwindcss

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI, Request
 from logging_request import log_request
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 @app.post("/")
@@ -9,4 +11,5 @@ async def read_root(request: Request):
     headers = dict(request.headers)
     body = await request.body()
     await log_request(request, "./logs")
-    return {"headers": headers, "body": body.decode()}
+    response = {"headers": headers, "body": body.decode()}
+    return templates.TemplateResponse("response.html", {"request": request, "response": response})
